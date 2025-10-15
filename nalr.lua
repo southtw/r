55dbc6624ab44122f987677e04ab6aba13d12310
southtw <109669438+southtw@users.noreply.github.com>
2025-10-16 00:08:12 +0800

    upd

--- nalr.lua
+++ nalr.lua
@@ -21,9 +21,7 @@ local MainSection = MainTab:NewSection("auto farm")
 MainSection:NewToggle("sword cutting", "Automatically farm sword cutting job", function(state)
     autoFarmEnabled = state
     if state then
-        print("enabled")
     else
-        print("disabled")
     end
 end)
 
@@ -57,133 +55,48 @@ local function onJobEnded()
     
     isProcessing = true
 
-    SendMessage(url, "job ended")
-
     wait(1)
 
-    SendMessage(url, "buying sd")
     for i = 1, 4 do
         local success, err = pcall(function()
             BuyItem:InvokeServer("SportsDrink")
         end)
         if not success then
-            SendMessage(url, "failed to buy sd")
         end
         wait(0.2)
     end
 
-    SendMessage(url, "consuming sd")
     for i = 1, 4 do
         local success, err = pcall(function()
             ConsumeFood:FireServer("SportsDrink")
         end)
         if not success then
-            SendMessage(url, "failed consuming sd")
         end
         wait(0.2)
     end
 
-    SendMessage(url, "tp sc")
     wait(0.5)
     local success, err = pcall(function()
         Warp:InvokeServer(Vector3.new(-1818, 63, 2055))
     end)
     if not success then
-        SendMessage(url, "failed to tp")
     end
 
     wait(1)
 
-    SendMessage(url, "start gooning")
     local success, err = pcall(function()
         StartJob:FireServer("SwordCuttingGame")
     end)
     if not success then
-        SendMessage(url, "failed to goon")
     end
 
-    SendMessage(url, "waiting for next goon")
     isProcessing = false
 end
 
 JobEnded.OnClientEvent:Connect(onJobEnded)
 
---/ Discord Webhook
-function SendMessage(url, message)
-    local http = game:GetService("HttpService")
-    local headers = {
-        ["Content-Type"] = "application/json"
-    }
-    local data = {
-        ["content"] = message
-    }
-    local body = http:JSONEncode(data)
-    local response = request({
-        Url = url,
-        Method = "POST",
-        Headers = headers,
-        Body = body
-    })
-end
-
---[[
-function SendMessageEMBED(url, embed)
-    local http = game:GetService("HttpService")
-    local headers = {
-        ["Content-Type"] = "application/json"
-    }
-    local data = {
-        ["embeds"] = {
-            {
-                ["title"] = embed.title,
-                ["description"] = embed.description,
-                ["color"] = embed.color,
-                ["fields"] = embed.fields,
-                ["footer"] = {
-                    ["text"] = embed.footer.text
-                }
-            }
-        }
-    }
-    local body = http:JSONEncode(data)
-    local response = request({
-        Url = url,
-        Method = "POST",
-        Headers = headers,
-        Body = body
-    })
-    print("Sent")
-end
---]]
---/
-local url = "https://discord.com/api/webhooks/1427848569985040414/dgFdkp_c6kUBcpGcxA8W-ZhKlFSipGbhipE0xX9_hnoi4yCTrePL7i-LGNT1FiEl9Mcd"
-
---[[
-local embed = {
-    ["title"] = "This is an embedded message",
-    ["description"] = "This message has an embed with fields and a footer",
-    ["color"] = 65280,
-    ["fields"] = {
-        {
-            ["name"] = "Field 1",
-            ["value"] = "This is the first field"
-        },
-        {
-            ["name"] = "Field 2",
-            ["value"] = "This is the second field"
-        }
-    },
-    ["footer"] = {
-        ["text"] = "This is the footer text"
-    }
-}
-SendMessageEMBED(url, embed)
---]]
-
 local nigga = Window:NewTab("info")
-local nisec = nigga:NewSection("start a job first")
+local nisec = nigga:NewSection("settings")
 nisec:NewKeybind("toggle ui", "kys", Enum.KeyCode.H, function()
 	Library:ToggleUI()
 end)
-
-SendMessage(url, "script loaded nigger")
